2e14a74674089a7edd35a83a2b6964c9e90cb873
ZhanatShengelbay <10541107@polimi.it>
2020-06-19 16:14:00 +0200

    ViewMain -> App

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -1,9 +1,19 @@
 package it.polimi.ingsw;
 
-public class App 
+import it.polimi.ingsw.view.MenuGUI;
+
+import javax.swing.*;
+
+public class App
 {
     public static void main( String[] args )
     {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MenuGUI();
+            }
+        });
 
     }
 }
